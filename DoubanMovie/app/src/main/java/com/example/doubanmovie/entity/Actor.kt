2838d007744c90286b9601d5441d5520d6ebff5d
lancerXXXX <2571021108@qq.com>
2021-07-20 21:33:02 +0800

    详情页可简单展示

---
+++ DoubanMovie/app/src/main/java/com/example/doubanmovie/entity/Actor.kt
@@ -0,0 +1,6 @@
+package com.example.doubanmovie.entity
+
+data class Actor(
+    val name: String,
+    val url: String
+)
---
+++ DoubanMovie/app/src/main/java/com/example/doubanmovie/entity/Director.kt
@@ -0,0 +1,6 @@
+package com.example.doubanmovie.entity
+
+data class Director(
+    val name: String,
+    val url: String
+)
