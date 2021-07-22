13f51165ca0bd6a30ba5dd9c388f34718c3100ae
lancerXXXX <2571021108@qq.com>
2021-07-22 18:45:17 +0800

    RecyclerView使用dataBinding

--- DoubanMovie2/app/src/main/java/com/example/doubanmovie/MovieCardAdapter.kt
+++ DoubanMovie2/app/src/main/java/com/example/doubanmovie/MovieCardAdapter.kt
@@ -3,47 +3,45 @@ package com.example.doubanmovie
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
-import android.widget.TextView
+import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.RecyclerView
 import coil.load
+import com.example.doubanmovie.databinding.MovieItemBinding
 import com.example.doubanmovie.entity.Episode
 
-class MovieCardAdapter(private val dataSet: List<Episode>) : RecyclerView.Adapter<MovieCardAdapter.ViewHolder>() {
+class MovieCardAdapter(private val dataSet: List<Episode>) :
+    RecyclerView.Adapter<MovieCardAdapter.ViewHolder>() {
 
     // listener
     private lateinit var onItemClickListener: OnItemClickListener
 
-    inner class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
-        val movieName: TextView = view.findViewById(R.id.movieName)
-        val movieRate: TextView = view.findViewById(R.id.movieRate)
-        val movieCover: ImageView = view.findViewById(R.id.movieCover)
+    inner class ViewHolder(itemView: View, private val movieItemBinding: MovieItemBinding) :
+        RecyclerView.ViewHolder(itemView) {
+        fun getBinding(): MovieItemBinding {
+            return movieItemBinding
+        }
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.movie_item, parent, false)
-        return ViewHolder(view)
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): ViewHolder {
+        val binding = DataBindingUtil.inflate<MovieItemBinding>(
+            LayoutInflater.from(parent.context), R.layout.movie_item, parent, false
+        )
+        return ViewHolder(binding.root, binding)
+
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        holder.movieName.text = dataSet[position].title
-        holder.movieRate.text = dataSet[position].rate
-        holder.movieCover.load(dataSet[position].cover)
-        // listener
-        holder.movieCover.setOnClickListener{
-            onItemClickListener?.onItemClick(holder.itemView, position)
-        }
-        holder.movieCover.setOnLongClickListener{
-            onItemClickListener?.onItemLongClick(holder.itemView, position)
-            true
-        }
-
+        holder.getBinding().episodeInfo = dataSet[position]
+        holder.getBinding().movieCover.load(dataSet[position].cover)
     }
 
     override fun getItemCount() = dataSet.size
 
     // listener
-    interface OnItemClickListener{
+    interface OnItemClickListener {
         fun onItemClick(view: View, position: Int)
         fun onItemLongClick(view: View, position: Int)
     }
