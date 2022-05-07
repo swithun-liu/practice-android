b8868cf662c37f76839d65e6d9e961cf3d72b429
LancerXXXX <2571021108@qq.com>
2022-05-07 10:11:06 +0800

    mac model

--- LEARN_COROUTINE_MAC/MyApplication/app/src/main/java/com/swithun/myapplication/MainActivity.kt
+++ LEARN_COROUTINE_MAC/MyApplication/app/src/main/java/com/swithun/myapplication/MainActivity.kt
@@ -3,16 +3,32 @@ package com.swithun.myapplication
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.swithun.myapplication.databinding.ActivityMainBinding
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
-    
+
     private lateinit var binding: ActivityMainBinding
+
+    private val mainScope = CoroutineScope(SupervisorJob() + Dispatchers.Main + Dispatchers.IO)
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
         binding = ActivityMainBinding.inflate(layoutInflater)
 
+        binding.btTest.setOnClickListener {
+            mainScope.launch {
+                test()
+            }
+        }
+
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+    }
+
+    private fun test() {
+        swithunLog("current thread is : ${Thread.currentThread().name}")
     }
 }
---
+++ LEARN_COROUTINE_MAC/MyApplication/app/src/main/java/com/swithun/myapplication/Util.kt
@@ -0,0 +1,7 @@
+package com.swithun.myapplication
+
+import android.util.Log
+
+fun swithunLog(text: String) {
+    Log.d("swithun-xxxx", text)
+}
