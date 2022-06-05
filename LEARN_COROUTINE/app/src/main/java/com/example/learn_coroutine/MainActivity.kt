fed5bea6e78ead6e4c91c61960f67845728ebc25
swithun <2571021108@qq.com>
2022-06-05 18:24:24 +0800

    swithun delay

--- LEARN_COROUTINE/app/src/main/java/com/example/learn_coroutine/MainActivity.kt
+++ LEARN_COROUTINE/app/src/main/java/com/example/learn_coroutine/MainActivity.kt
@@ -3,27 +3,43 @@ package com.example.learn_coroutine
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.learn_coroutine.databinding.ActivityMainBinding
+import kotlinx.coroutines.runBlocking
+import swithunPrint
+import java.util.concurrent.Executors
+import java.util.concurrent.TimeUnit
 import kotlin.coroutines.*
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private val executor = Executors.newScheduledThreadPool(1) { runnable ->
+        Thread(runnable, "Scheduler").apply { isDaemon = true }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         binding = ActivityMainBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
+        runBlocking {
+            swithunPrint("[THREAD-${Thread.currentThread().name}] before swithunDelay")
+            swithunDelay(2000)
+            swithunPrint("[THREAD-${Thread.currentThread().name}] after swithunDelay")
+        }
 
-        val continuation = suspend {
-            println("In Coroutine.")
-            5
-        }.startCoroutine(object : Continuation<Int> {
-            override fun resumeWith(result: Result<Int>) {
-                println("Coroutine End: $result")
-            }
+    }
 
-            override val context = EmptyCoroutineContext
-        })
+    suspend fun swithunDelay(time: Long, unit: TimeUnit = TimeUnit.MILLISECONDS) {
+        if (time <= 0) {
+            return
+        }
+        suspendCoroutine<Unit> { continuation ->
+            executor.schedule({
+                swithunPrint("[THREAD-${Thread.currentThread()}] then continuation resume")
+                swithunPrint("[THREAD-${Thread.currentThread().name}] then continuation resume")
+                continuation.resume(Unit)
+            },
+            time, unit)
+        }
     }
 }
