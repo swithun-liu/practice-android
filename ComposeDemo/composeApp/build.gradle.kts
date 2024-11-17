721490242bb7f944dfdf61adbd77119a955eca33
swithun <2571021108@qq.com>
2024-11-17 20:48:56 +0800

    页面跳转

--- ComposeDemo/composeApp/build.gradle.kts
+++ ComposeDemo/composeApp/build.gradle.kts
@@ -69,6 +69,7 @@ kotlin {
             implementation(libs.androidx.lifecycle.viewmodel)
             implementation(libs.androidx.lifecycle.runtime.compose)
             implementation("org.jetbrains.kotlinx:kotlinx-datetime:0.6.0")
+            implementation(libs.navigation.compose)
         }
         desktopMain.dependencies {
             implementation(compose.desktop.currentOs)
--- ComposeDemo/composeApp/src/commonMain/kotlin/compose/project/demo/App.kt
+++ ComposeDemo/composeApp/src/commonMain/kotlin/compose/project/demo/App.kt
@@ -1,7 +1,10 @@
 package compose.project.demo
 
 import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.slideIn
+import androidx.compose.animation.slideOut
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -17,8 +20,15 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import org.jetbrains.compose.resources.painterResource
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
@@ -37,63 +47,111 @@ import org.jetbrains.compose.resources.DrawableResource
 @Preview
 fun App() {
     MaterialTheme {
-        var showContent by remember { mutableStateOf(false) }
-        var locaton by remember { mutableStateOf("Europe/Paris") }
-        var timeAtLocation by remember { mutableStateOf("No location selected") }
-        var showCountries by remember { mutableStateOf(false) }
-        Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
-            Text(timeAtLocation)
-            TextField(value = locaton, onValueChange = { locaton = it })
-            Button(onClick = { timeAtLocation = currentTimeAt(locaton, TimeZone.currentSystemDefault()) ?: "Invalid Location" }) {
-                Text("Show Time At Location")
+        val navigationController = rememberNavController()
+        NavHost(
+            navController = navigationController,
+            startDestination ="first_screen",
+            enterTransition = { slideIn(initialOffset = { IntOffset(it.width, 0) }) },
+            exitTransition = { slideOut(targetOffset = { IntOffset(0, 0) } ) }
+        ) {
+            composable("first_screen") {
+                FirstScreen(navigationController)
             }
-            Button(modifier = Modifier.padding(start = 20.dp, top = 10.dp),
-                onClick = { showCountries = !showCountries }) {
-                Text("Select Location")
-            }
-            Row(modifier = Modifier.padding(start = 20.dp)) {
-                DropdownMenu(
-                    expanded = showCountries,
-                    onDismissRequest = { showCountries = false }
-                ) {
-                    countries().forEach { (name, zone, image) ->
-                        DropdownMenuItem(
-                            onClick = {
-                                timeAtLocation = currentTimeAt(name, zone)
-                                showCountries = false
-                            }
-                        ) {
-                            Row(verticalAlignment = Alignment.CenterVertically) {
-                                Image(
-                                    painterResource(image),
-                                    modifier = Modifier.size(50.dp).padding(end = 10.dp),
-                                    contentDescription = "$name flag"
-                                )
-                                Text(name)
-                            }
-                        }
-                    }
-                }
-            }
-            Text(
-                text = "Today's date is ${todaysDate()}",
-                modifier = Modifier.padding(20.dp),
-                fontSize = 24.sp,
-                textAlign = TextAlign.Center
-            )
-            Button(onClick = { showContent = !showContent }) {
-                Text("Click me!")
-            }
-            AnimatedVisibility(showContent) {
-                val greeting = remember { Greeting().greet() }
-                Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
-                    Image(painterResource(Res.drawable.compose_multiplatform), null)
-                    Text("Compose: $greeting")
-                }
+            composable("second_screen") {
+                SecondScreen(navigationController)
             }
         }
     }
 }
+
+@Composable
+fun FirstScreen(
+    navigationController: NavHostController,
+    viewModel: FirstScreenViewModel = viewModel { FirstScreenViewModel() }
+) {
+    val uiState = viewModel.uiState.collectAsStateWithLifecycle()
+    Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally, verticalArrangement = Arrangement.Top) {
+        Button(onClick = { viewModel.changeIsShowContent() }) {
+            Text("Click me!")
+        }
+        Button(onClick = {
+            navigationController.navigate("second_screen")
+        }) {
+            Text("nav to 2")
+        }
+        TextField(value = uiState.value.location, onValueChange = { viewModel.changeLocation(it) })
+        AnimatedVisibility(uiState.value.showContent) {
+            val greeting = remember { Greeting().greet() }
+            Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
+                Image(painterResource(Res.drawable.compose_multiplatform), null)
+                Text("Compose: $greeting")
+            }
+        }
+    }
+}
+
+@Composable
+fun SecondScreen(navigationController: NavHostController) {
+    var showContent by remember { mutableStateOf(false) }
+    var locaton by remember { mutableStateOf("Europe/Paris") }
+    var timeAtLocation by remember { mutableStateOf("No location selected") }
+    var showCountries by remember { mutableStateOf(false) }
+    Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
+        Text(timeAtLocation)
+        TextField(value = locaton, onValueChange = { locaton = it })
+        Button(onClick = { timeAtLocation = currentTimeAt(locaton, TimeZone.currentSystemDefault()) ?: "Invalid Location" }) {
+            Text("Show Time At Location")
+        }
+        Button(onClick = { navigationController.popBackStack() }) {
+            Text("Back")
+        }
+        Button(modifier = Modifier.padding(start = 20.dp, top = 10.dp),
+            onClick = { showCountries = !showCountries }) {
+            Text("Select Location")
+        }
+        Row(modifier = Modifier.padding(start = 20.dp)) {
+            DropdownMenu(
+                expanded = showCountries,
+                onDismissRequest = { showCountries = false }
+            ) {
+                countries().forEach { (name, zone, image) ->
+                    DropdownMenuItem(
+                        onClick = {
+                            timeAtLocation = currentTimeAt(name, zone)
+                            showCountries = false
+                        }
+                    ) {
+                        Row(verticalAlignment = Alignment.CenterVertically) {
+                            Image(
+                                painterResource(image),
+                                modifier = Modifier.size(50.dp).padding(end = 10.dp),
+                                contentDescription = "$name flag"
+                            )
+                            Text(name)
+                        }
+                    }
+                }
+            }
+        }
+        Text(
+            text = "Today's date is ${todaysDate()}",
+            modifier = Modifier.padding(20.dp),
+            fontSize = 24.sp,
+            textAlign = TextAlign.Center
+        )
+        Button(onClick = { showContent = !showContent }) {
+            Text("Click me!")
+        }
+        AnimatedVisibility(showContent) {
+            val greeting = remember { Greeting().greet() }
+            Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
+                Image(painterResource(Res.drawable.compose_multiplatform), null)
+                Text("Compose: $greeting")
+            }
+        }
+    }
+}
+
 
 data class Country(val name: String, val zone: TimeZone, val image: DrawableResource)
 
